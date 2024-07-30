510c34ae1e0f423c3392a2a56105a7d851975fc1
Michal Paduch <mipa@noitso.dk>
2024-07-30 21:36:15 +0200

    Exercise 1 done

--- ex_001/ex_001.spec.ts
+++ ex_001/ex_001.spec.ts
@@ -1,9 +1,16 @@
-import { testFn } from "./ex_001";
+import { sumOfNumbers } from "./ex_001";
 
-describe('ex_001', () => {
-    it('should work', () => {
-        const result = testFn();
+describe("ex_001", () => {
+    // 2, 3, 4, 6, 8, 9
+    it("1 case - 2, 3 in 10", () => {
+        const result = sumOfNumbers(2, 3, 10);
 
-        expect(result).toBe('done');
+        expect(result).toBe(32);
+    });
+
+    it("exercise case - 3, 5 in 1000", () => {
+        const result = sumOfNumbers(3, 5, 1000);
+
+        expect(result).toBe(233168);
     });
 });
--- ex_001/ex_001.ts
+++ ex_001/ex_001.ts
@@ -1,3 +1,10 @@
-export function testFn() {
-    return "done";
+export function sumOfNumbers(x: number, y: number, to: number): number {
+    let result = 0;
+    for (let currentDigit = 1; currentDigit < to; currentDigit++) {
+        if (currentDigit % x === 0 || currentDigit % y === 0) {
+            result += currentDigit;
+        }
+    }
+
+    return result;
 }
